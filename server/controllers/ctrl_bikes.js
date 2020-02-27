69e169986c4d2bfe2236c9f8b9b0bc21d4bfc6d2
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2020-02-26 23:22:32 -0800

    Update ctrl_bikes.js
    Adding "New bike" function

--- server/controllers/ctrl_bikes.js
+++ server/controllers/ctrl_bikes.js
@@ -1,1 +1,19 @@
-Add your controller code here and rename the file 
+const mongoose = require('mongoose');
+const MongModels = require('../models/model_bike');
+const flash = require('express-flash');
+const Bike = MongModels.Bike;
+
+module.exports = {
+    newbike: function (req, res){
+        console.log ("creating a new Bike record");
+        _newbike = req.body;
+        console.log(_newbike);
+        bike = new Bike(_newbike);
+        bike.save()
+            .then(saveResult => res.JSON(saveResult))
+            .catch(err => {
+                console.log("Error crating a new Restaurant record");
+                res.json(err);
+            });
+    }
+}
